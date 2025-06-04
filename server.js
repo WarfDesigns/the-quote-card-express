a9d47a8300e67e0724c8a6a030d1f05cc7326e03
Brent Warf <brentwarf252@msn.com>
2025-06-04 11:02:12 -0500

    Created server.js and index.html.

---
+++ server.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const port = 8080;
+app.use(express.static("./public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
