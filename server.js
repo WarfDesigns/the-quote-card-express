683ce83777c7f6248150619a7193c422c500a740
Brent Warf <brentwarf252@msn.com>
2025-06-04 11:31:55 -0500

    express set up to serve a single page.

--- server.js
+++ server.js
@@ -2,7 +2,11 @@
 
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = 5555;
 app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.listen(port, () => {
+    console.log(`Server is running http://localhost:${port}`);
+    console.log("Press Ctrl+C to end this process.");
+});
