ffc7756a22c44fd5d3c75c9e00dd740f2496d06c
Brent Warf <brentwarf252@msn.com>
2025-06-09 11:17:02 -0500

    Added content to index.js file. Changed the quote in the index.html file. Modified package.json according to part 3 of the assignment. Added devdepencies according to part 3 of the assignment to package-lock.json.

---
+++ public/js/index.js
@@ -0,0 +1,34 @@
+"use strict"
+
+const elements = {
+    quote: document.getElementById("quote"),
+    author: document.getElementById("author"),
+};
+const quotes = [
+    {
+        quote: "Remorseless Labor Conquers All",
+        author: "Virgil",
+    },
+
+    {
+        quote: "I can do all things through Christ who strengthens me.",
+        author: "Philippians 4:13",
+    },
+
+    {
+        quote: "Not all those who wander are lost.",
+        author: "J.R.R. Tolkien",
+    }
+]
+function loopThroughQuotes() {
+    let quoteIndex = 0;
+    setInterval(() => {
+        if (quoteIndex < quotes.length) {
+            elements.quote.textContent = quotes[quoteIndex].quote;
+            elements.author.textContent = quotes[quoteIndex].author;
+            quoteIndex++;
+        } else {
+            quoteIndex = 0;
+        }
+    }, 3000);
+}
